e15a2c44342826b6c8bc801975f27e27390697d8
Kelechi Ikegwu <kelechi.ikegwu@gmail.com>
2022-05-24 21:54:17 -0400

    Include Published Edits

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #! /bin/bash 
 
+# Compile main doc
 pdflatex main.tex
 
+# For aux files creat bbl files
 for f in *.aux; do bibtex ${f}; done
 
 
 #bibtex main.aux
 
+# latex again twice for references and citations
 pdflatex main.tex
 pdflatex main.tex
+
+
+# produces main.pdf which contains my dissertation
